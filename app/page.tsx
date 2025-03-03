d7e695e055422e79e3f6ab6e33bacb90c905d5f1
zihan teng <14494049+tzh943855696@user.noreply.gitee.com>
2025-03-03 08:41:17 +0800

    feat: header

--- app/page.tsx
+++ app/page.tsx
@@ -1,101 +1,120 @@
 import Image from "next/image";
+import { Yesteryear } from "next/font/google";
+import * as motion from "motion/react-client";
+import "./index.css";
+
+const yesteryear = Yesteryear({
+  weight: "400",
+  subsets: ["latin"],
+});
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+    <>
+      <nav className="justify-centerpy-2 flex w-full select-none pt-6 font-light md:px-28 md:pb-2">
+        <div className="container flex flex-col items-center justify-between md:flex-row">
+          <div className={`text-2xl drop-shadow-2xl ${yesteryear.className}`}>
+            <a href="/web/20241130224736mp_/https://www.erichuang.art/">
+              Locke Teng
+            </a>
+          </div>
+          <div className="nav-links flex gap-x-8 text-xs md:text-base">
+            <span className="cursor-pointer">About</span>
+            <span className="cursor-pointer">Skills</span>
+            <span className="cursor-pointer">Contact</span>
+          </div>
         </div>
+      </nav>
+
+      <div className="fixed inset-0 -z-10 h-full w-full bg-white bg-[linear-gradient(to_right,#fafafa_1px,transparent_1px),linear-gradient(to_bottom,#fafafa_1px,transparent_1px)] bg-[size:14px_24px]"></div>
+
+      <main className="container">
+        <section className="my-8 flex flex-col justify-center items-center">
+          <motion.h1
+            initial={{ opacity: 0, translateY: 30 }}
+            whileInView={{ opacity: 1, translateY: 0 }}
+            transition={{
+              duration: 0.5,
+            }}
+            className="mb-4 text-[1.4rem] md:text-[2rem] relative"
+          >
+            Hi I'm Locke TENG!👋
+          </motion.h1>
+
+          <div className="overflow-hidden rounded-full p-3 md:p-4">
+            <motion.div>
+              <video
+                className="h-[170px] w-[170px] md:h-[190px] md:w-[190px]"
+                muted
+                autoPlay
+                loop
+                playsInline
+              >
+                <source src={"/animoji.mp4"} type="video/mp4" />
+                Your browser does not support the video tag.
+              </video>
+            </motion.div>
+          </div>
+
+          <motion.h1
+            initial={{ opacity: 0, translateY: 30 }}
+            whileInView={{ opacity: 1, translateY: 0 }}
+            transition={{
+              delay: 0.3,
+              duration: 0.5,
+            }}
+            className="relative "
+          >
+            Developer 🧑🏻‍💻
+          </motion.h1>
+
+          <motion.h1
+            initial={{ opacity: 0, translateY: 30 }}
+            whileInView={{ opacity: 1, translateY: 0 }}
+            transition={{
+              delay: 0.4,
+              duration: 0.5,
+            }}
+            className="relative "
+          >
+            INTJ 📚
+          </motion.h1>
+
+          <div className="my-12 flex w-full flex-col gap-2 text-center lg:w-[50%]">
+            <motion.div
+              initial={{ opacity: 0, translateY: 30 }}
+              whileInView={{ opacity: 1, translateY: 0 }}
+              transition={{
+                delay: 0.5,
+                duration: 0.5,
+              }}
+              className="relative"
+            >
+              <p>Welcome to my personal page!</p>
+            </motion.div>
+
+            <motion.div
+              initial={{ opacity: 0, translateY: 30 }}
+              whileInView={{ opacity: 1, translateY: 0 }}
+              transition={{
+                delay: 0.6,
+                duration: 0.5,
+              }}
+              className="relative"
+            >
+              <p>
+                Just a ✨ Front-End developer who loves to build something cool.
+              </p>
+            </motion.div>
+          </div>
+
+          <div className="my-8">
+            <ul>
+              <li></li>
+            </ul>
+          </div>
+        </section>
       </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+    </>
   );
 }
