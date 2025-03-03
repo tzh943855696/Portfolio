28a97c3b6a3eb1c98150a29501bc1dbee0094cd2
zihan teng <14494049+tzh943855696@user.noreply.gitee.com>
2025-03-03 08:41:17 +0800

    feat: header

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,16 +1,12 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Montserrat } from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+const montserrat = Montserrat({
+  weight: '600',
+  display: 'fallback',
+  subsets: ['cyrillic']
+})
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -24,8 +20,12 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      </head>
+
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${montserrat.className} antialiased`}
       >
         {children}
       </body>
